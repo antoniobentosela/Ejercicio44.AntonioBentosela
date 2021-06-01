3f82c624e205cf8363574860295c3d5a95361f3e
antoniobentosela <52534695+antoniobentosela@users.noreply.github.com>
2021-05-31 21:09:43 -0300

    Primera subida

---
+++ RepasoPrograII-cascara/RepasoPrograII20210531/Application.Files/Text/Text.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Files.Text
+{
+    public class Text<T> : IFile<T>
+    {
+        public bool Read(string file, out T data)
+        {
+           throw new NotImplementedException();
+        }
+
+        public bool Save(string file, T data)
+        {
+            StreamWriter sw = new StreamWriter(file);
+            if (data != null)
+            {
+                sw.Write(data);
+                sw.Close();
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ RepasoPrograII-cascara/RepasoPrograII20210531/Application.Files/Xml/Xml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Application.Files.Xml
+{
+    public class Xml<T> : IFile<T> where T : class, new()
+    {
+        //TODO usar aca excepciones (try, catch) para validar el serialize y deserializa. Encerrando los using completos
+        public bool Read(string file, out T data)
+        {                    
+            using (XmlTextReader xmlTextReader = new XmlTextReader(file))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                data = (T)serializer.Deserialize(xmlTextReader);
+                return serializer.CanDeserialize(xmlTextReader);       
+            }
+        }
+
+        public bool Save(string file, T data)
+        {
+            using (XmlTextWriter xmlTextWriter = new XmlTextWriter(file, Encoding.UTF8))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));               
+                if (data != null)
+                {
+                    serializer.Serialize(xmlTextWriter, data);
+                    return true;
+                }
+                else 
+                {
+                    return false;
+                }        
+            }
+        }
+    }
+}
